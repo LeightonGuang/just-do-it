da875746bf7488522b93d4a9a52478912eef86b0
LeightonGuang <leighton.guang@icloud.com>
2026-09-15 14:15:42 +0100

    feat: new kanban with dotted background

---
+++ src/components/Kanban.tsx
@@ -0,0 +1,23 @@
+const Kanban = () => {
+  return (
+    <section className="dot-grid size-full min-h-screen border border-border p-8">
+      <h1 className="mb-8 leading-4 font-medium text-text">Project Name</h1>
+
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        <article className="border border-border bg-card p-2">
+          <h2 className="leading-8 font-semibold text-text">TODO</h2>
+        </article>
+
+        <article className="border border-border bg-card p-2">
+          <h2 className="leading-8 font-semibold text-text">IN PROGRESS</h2>
+        </article>
+
+        <article className="border border-border bg-card p-2">
+          <h2 className="leading-8 font-semibold text-text">DONE</h2>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Kanban;
