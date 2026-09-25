539899211f7693852bc22e3729aeb40f72adc779
LeightonGuang <leighton.guang@icloud.com>
2026-09-25 02:46:30 +0100

    style: adjust spacing for mobile view

--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -34,7 +34,7 @@ const Kanban = ({ doId }: { doId: string | null }) => {
     <section className="dot-grid min-h-full p-4 md:p-8">
       <h1 className="leading-4 font-medium text-text">{project.name}</h1>
 
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] mt-4 md:mt-8 gap-8">
+      <div className="mt-4 grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4 md:mt-8 md:gap-8">
         {columns.map((column) => {
           const columnDos = dos.filter(
             (doItem) => doItem.column_id === column.id,
