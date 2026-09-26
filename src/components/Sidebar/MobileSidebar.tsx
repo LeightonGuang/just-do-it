0549710d65f57251300787a7f262cbb6ab3cd1b5
LeightonGuang <leighton.guang@icloud.com>
2026-09-26 14:50:37 +0100

    style: add padding on top so content doesn't touch the top of the sidebar

--- src/components/Sidebar/MobileSidebar.tsx
+++ src/components/Sidebar/MobileSidebar.tsx
@@ -6,16 +6,18 @@ import ProjectsSidebar from "./ProjectsSidebar";
 
 const MobileSidebar = ({ className }: { className?: string }) => {
   return (
-    <aside className={twMerge("mb-4 w-full", className)}>
-      <h1 className="m-4 text-lg font-medium whitespace-nowrap text-orange-500 uppercase">
-        <a href="/" className="hover:underline">
-          Just Do it
-        </a>
-      </h1>
+    <aside className={twMerge("w-full overflow-hidden pt-4", className)}>
+      <div className="h-full overflow-y-auto ">
+        <h1 className="text-lg font-medium whitespace-nowrap mx-4 text-orange-500 uppercase">
+          <a href="/" className="hover:underline">
+            Just Do it
+          </a>
+        </h1>
 
-      <DailyQuote className="m-4 md:mx-0" />
-      <DoSidebar />
-      <ProjectsSidebar />
+        <DailyQuote className="m-4 md:mx-0" />
+        <DoSidebar />
+        <ProjectsSidebar />
+      </div>
     </aside>
   );
 };
